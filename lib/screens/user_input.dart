5eb2f048f6d8de54877591b8f4d0e4b89a09d6de
ruan.valente <ruan.valente@celk.net>
2021-08-09 16:39:31 -0300

    refactor: refactor UserInput for StateFullWidget

--- lib/screens/user_input.dart
+++ lib/screens/user_input.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class UserInput extends StatelessWidget {
+class UserInput extends StatefulWidget {
   const UserInput({Key? key}) : super(key: key);
+
+  @override
+  _UserInputState createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+  final textInputUserController = TextEditingController();
+
+  @override
+  void dispose() {
+    textInputUserController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +23,39 @@ class UserInput extends StatelessWidget {
             child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text('😀', style: TextStyle(fontSize: 36)),
-        TextField(
-          decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'como eu posso chamar você ?'),
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('😀', style: TextStyle(fontSize: 36)),
         ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Text(
+            'Como podemos chamar você?',
+            style: TextStyle(fontSize: 24, color: Colors.black54),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          width: 350,
+          child: TextField(
+            autofocus: true,
+            controller: textInputUserController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), hintText: 'Digite seu nome'),
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 80,
+          child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/welcome');
+                    print(textInputUserController.text);
+                  },
+                  child: Text('Continuar'))),
+        )
       ],
     )));
   }
