1575dfc048943bdfec3cf34747ed1dcd49d03fcf
ruan.valente <ruan.valente@celk.net>
2021-08-10 08:33:59 -0300

    refactor: refactor TitleApp Widget

--- lib/components/title.dart
+++ lib/components/title.dart
@@ -3,15 +3,21 @@ import 'package:flutter/material.dart';
 class TitleApp extends StatelessWidget {
   final String title;
   final double size;
+  final FontWeight? weight;
+  final Color? color;
   const TitleApp(
-      {Key? key, required String this.title, required double this.size})
+      {Key? key,
+      required String this.title,
+      required double this.size,
+      this.weight,
+      this.color})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(title,
         style: TextStyle(
-            fontSize: size, fontWeight: FontWeight.bold, color: Colors.black54),
+            fontSize: size, fontWeight: weight, color: color ?? Colors.black54),
         textAlign: TextAlign.center);
   }
 }
